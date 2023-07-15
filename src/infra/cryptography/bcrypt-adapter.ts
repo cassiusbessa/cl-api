8454b641ef61455a7c04877bd00fa7a7f58a85cf
cassiusbessa <cassiusbessa@gmail.com>
2023-07-14 23:44:09 -0300

    fix: :bug: fix colums name and null value

---
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -0,0 +1,11 @@
+import bcrypt from 'bcrypt'
+import { Hasher } from 'src/data/protocols/cryptography'
+
+export class BcryptAdapter implements Hasher {
+  constructor (private readonly salt: number) {}
+
+  async hash (value: string): Promise<string> {
+    const hash = await bcrypt.hash(value, this.salt)
+    return hash
+  }
+}
