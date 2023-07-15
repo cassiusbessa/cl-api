bfca2f52126cacbca077a6957fbcc2fed0c857e5
cassiusbessa <cassiusbessa@gmail.com>
2023-07-15 01:04:21 -0300

    refactor: :art: remove token from signup controller

--- src/presentation/controllers/user/signup-controller.ts
+++ src/presentation/controllers/user/signup-controller.ts
@@ -1,5 +1,4 @@
 import { AddAccount } from "src/domain/usecases/users/add-account"
-import { Authentication } from "src/domain/usecases/users/authentication"
 import { EmailInUseError } from "src/presentation/errors"
 import { Controller } from "src/presentation/protocols/controller"
 import { HttpRequest, HttpResponse } from "src/presentation/protocols/http"
@@ -7,7 +6,7 @@ import { badRequest, forbidden, ok, serverError } from "src/presentation/protoco
 import { Validator } from "src/presentation/protocols/validator"
 
 export class SignUpController implements Controller {
-  constructor (private readonly addAccount: AddAccount, private readonly validator: Validator, private readonly authentication: Authentication) {}
+  constructor (private readonly addAccount: AddAccount, private readonly validator: Validator) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
 
@@ -21,10 +20,8 @@ export class SignUpController implements Controller {
 
       const account = await this.addAccount.add({fullName, email, password})
       if(!account) return forbidden(new EmailInUseError())
-
   
-      const accessToken = await this.authentication.auth({email, password})  
-      return ok({account, accessToken})
+      return ok({account})
       
     } catch (error) {
       return serverError(error as Error)
